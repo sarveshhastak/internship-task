920d63f636c4a0f0ea605e5b7007842339065792
sarvesh hastak <sarveshhastak1309@gmail.com>
2025-06-06 17:06:21 +0530

    vercel json update

--- src/pages/home/Section_Eight.jsx
+++ src/pages/home/Section_Eight.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import pic2 from "./../../../assets/screenshot 2.png"
 import Section_Eight_Carousel from './Section_Eight_Carousel'
 const Section_Eight = () => {
     return <>
--- src/pages/home/Section_Two.jsx
+++ src/pages/home/Section_Two.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import pic from "./../../../assets/screenshot 2.png"
 import Srction_Two_Carousel from './Section_Two_Carousel'
 const Section_Two = () => {
     return <>
